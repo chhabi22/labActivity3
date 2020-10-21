ac030cdaf63e051bc4aa6cffc8108475bfe51c9f
Chhabi <chhabigupta22@gmail.com>
2020-10-21 10:28:15 -0400

    finished

---
+++ index.js
@@ -0,0 +1,8 @@
+function changeText() {
+    document.getElementById("text").innerHTML = "God's Image";
+  }
+
+  function popp() {
+    alert("This is an Alert Message");
+  }
+  
--- program.js
+++ program.js
@@ -1,7 +1,18 @@
-var http = require("http");
+// var http = require("http");
+// var url = require("url");
 
-	http.createServer(function(request, response) {
-	response.writeHead(200,  {"Content-Type":  "text/plain"});
-	response.write("Hello World");
-	response.end();
-	}).listen(8080);
+// 	function start(){
+// 		function onRequest(request, response){
+// 			var pathname = url.parse(request.url).pathname;
+// 		console.log("Req recieved")
+// 	response.writeHead(200,  {"Content-Type":  "text/plain"});
+// 	response.write("Hello World");
+// 	response.end();
+// 	};
+
+// 	http.createServer(onRequest).listen(8080);
+
+// 	console.log("server has started");
+// }
+
+// exports.start = start;
